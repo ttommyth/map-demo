744babe0f1bf529c1bdf2ebfda86f0f9bc87c8c2
ttommyth <ttommyth@gmail.com>
2023-11-20 11:53:44 +0800

    style: updated header style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <aside className='relative bg-base-50 rounded-b-3xl sm:rounded-bl-none sm:rounded-r-3xl sm:w-1/3 min-w-[320px] sm:h-screen w-screen shadow-md flex flex-col'>
       <span className='flex flex-row justify-center items-center p-2 sm:p-4 gap-2'>
         <div className='relative w-8 h-8 sm:w-24 sm:h-24'><Image src={"/images/icon.png"} fill={true} alt='icon'/></div>
-        <h1>Map Demo</h1>
+        <h1 className='bg-clip-text bg-gradient-to-br from-primary-500 from-20% to-secondary-500 font-black text-transparent'>Map Demo</h1>
       </span>
       <RouteCheckSide />
       <div className='grow'></div>
